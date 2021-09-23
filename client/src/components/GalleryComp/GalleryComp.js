e9c5ec9b55be8e525fa4501ecd8fa8c2c007c6c5
Anthony Parrino <anthony21parrino21@gmail.com>
2021-09-22 20:17:34 -0400

    working on buy button for gallery

--- client/src/components/GalleryComp/GalleryComp.js
+++ client/src/components/GalleryComp/GalleryComp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './galleryComp.css';
 import Buddah from '../../paintings/Buddah.jpg';
 import Elephant from '../../paintings/Elephant.jpg';
@@ -25,144 +25,148 @@ import Zuzu from '../../paintings/Zuzu.JPG';
 
 function GalleryComp() {
 
-    let paintings = [
+    // let paintings = [
+    const [allArt, setAllArt] = useState([
+
         {
-            id: 0,
+            id: '0',
             name: 'Mr. White',
             price: 5000,
             src: Walter,
         },
         {
-            id: 1,
+            id: '1',
             name: 'Elephant',
             price: 'SOLD',
             src: Elephant,
         },
         {
-            id: 2,
+            id: '2',
             name: 'Marshal',
             price: 2000,
             src: Eminem,
         },
         {
-            id: 3,
+            id: '3',
             name: 'The End',
             price: 3000,
             src: Endgame,
         },
         {
-            id: 4,
+            id: '4',
             name: 'Float',
             price: 'SOLD',
             src: Float,
         },
         {
-            id: 5,
+            id: '5',
             name: 'Frank',
             price: 'SOLD',
             src: Frank,
         },
         {
-            id: 6,
+            id: '6',
             name: 'Aliens',
             price: 2000,
             src: Georgio,
         },
         {
-            id: 7,
+            id: '7',
             name: 'Baby Groot',
             price: 2000,
             src: Groot,
         },
         {
-            id: 8,
+            id: '8',
             name: 'Horse',
             price: 'Work In Progress',
             src: Horse,
         },
         {
-            id: 9,
+            id: '9',
             name: 'Jellyfish',
             price: 500,
             src: Jellyfish,
         },
         {
-            id: 10,
+            id: '10',
             name: 'Job',
             price: 3000,
             src: Jobs,
         },
         {
-            id: 11,
+            id: '11',
             name: 'Leopard',
             price: 2000,
             src: Leopard,
         },
         {
-            id: 12,
+            id: '12',
             name: 'Migrate',
             price: 400,
             src: Migration,
         },
         {
-            id: 13,
+            id: '13',
             name: 'Smash',
             price: 2500,
             src: Smash,
         },
         {
-            id: 14,
+            id: '14',
             name: 'Mr. Soprano',
             price: 3000,
             src: Sopranos,
         },
         {
-            id: 15,
+            id: '15',
             name: 'The Stooges',
             price: 'SOLD',
             src: Stooges1,
         },
         {
-            id: 16,
+            id: '16',
             name: 'The Stooges 2',
             price: 'SOLD',
             src: Stooges2,
         },
         {
-            id: 17,
+            id: '17',
             name: 'The Stooges 3',
             price: 'SOLD',
             src: Stooges3,
         },
         {
-            id: 18,
+            id: '18',
             name: 'The Stooges 4',
             price: 'SOLD',
             src: Stooges4,
         },
         {
-            id: 19,
+            id: '19',
             name: 'Coral',
             price: 3000,
             src: Turtles,
         },
         {
-            id: 20,
+            id: '20',
             name: 'The Buddah',
             price: 'SOLD',
             src: Buddah,
         },
         {
-            id: 21,
+            id: '21',
             name: 'Zuuuu',
             price: 'SOLD',
             src: Zuzu,
         },
-    ]
+    ])
 
     function buyPainting(e) {
-        let clicked = e.target.id
-        console.log(clicked)
+        let clicked = e.target
+        let buyPainting = []
+        buyPainting.push(allArt.find(art => art.id === clicked.id))
+        console.log('this', buyPainting)
     }
 
     return (
@@ -172,14 +176,26 @@ function GalleryComp() {
                 <p>All Originals Artwork, Hand Painted With Oils On Canvas By Anthony Parrino</p>
             </div>
             <div>
-                {paintings.map(paint => (
+                {allArt.map(paint => (
                     <div key={paint.id} className='painting-container'>
                         <h3>{paint.name}</h3>
-                        {paint.price === 'SOLD' ? <p className='painting-sold'>{paint.price}</p> : <p className='painting-price'>{paint.price}<strong className='painting-price'> USD</strong></p>}
-                        <img className='paintings' src={paint.src} alt='art'></img>
-                        <div>
-                            <button id={paint.id} className='galleryBuyBtn' onClick={buyPainting}>BUY</button>
-                        </div>
+                        {paint.price === 'SOLD' ?
+                            <div>
+                                <p className='painting-sold'>{paint.price}</p>
+                                <img className='paintings' src={paint.src} alt='art'></img>
+                                <div>
+                                    <button disabled id={paint.id} className='galleryBuyBtnSold' onClick={buyPainting}>X</button>
+                                </div>
+                            </div>
+                            :
+                            <div>
+                                <p className='painting-price'>{paint.price}<strong className='painting-price'> USD</strong></p>
+                                <img className='paintings' src={paint.src} alt='art'></img>
+                                <div>
+                                    <button id={paint.id} className='galleryBuyBtn' onClick={buyPainting}>ADD TO CART</button>
+                                </div>
+                            </div>
+                        }
                     </div>
                 ))}
             </div>
